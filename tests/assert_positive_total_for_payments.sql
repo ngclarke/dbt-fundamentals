4187bedb2666486bd1364835392965af983bb294
Nikole Clarke <nikole.clarke@scottscheapflights.com>
2022-04-11 21:05:07 +0000

    pre-deployment commit

---
+++ tests/assert_positive_total_for_payments.sql
@@ -0,0 +1,11 @@
+with payments as (
+    select * from {{ref('stg_payments')}}
+)
+-- Refunds have a negative amount, so the total amount should always be >= 0.
+-- Therefore return records where this isn't true to make the test fail.
+select
+    order_id,
+    sum(amount) as total_amount
+from payments
+group by order_id
+having not(total_amount >= 0)
